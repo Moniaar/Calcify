af9f241674d569114ee54fa8ea76a3712fa8e26d
Moniaar <www.omniaahmed1562001@gmail.com>
2025-03-24 14:18:02 +0200

    Debugging into an order

--- database/setup.sql
+++ database/setup.sql
@@ -12,8 +12,8 @@ CREATE TABLE IF NOT EXISTS products (
   name TEXT NOT NULL,
   price REAL NOT NULL,
   stock INTEGER NOT NULL,
-  storage_location TEXT NOT NULL -- New column: "Medical Supplements" or "Laboratory Supplements",
-  ALTER TABLE products ADD COLUMN cost REAL;
+  storage_location TEXT NOT NULL,
+  cost REAL -- New column: "Medical Supplements" or "Laboratory Supplements"
 );
 
 CREATE TABLE IF NOT EXISTS invoices (
